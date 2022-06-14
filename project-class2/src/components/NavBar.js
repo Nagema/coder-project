7a69637c923de7beb80dc7d99ec828487db26b17
angelica <angelica.cavero@gmail.com>
2022-06-14 17:20:04 +0200

    Fleamarket project - Cavero

---
+++ project-class2/src/components/NavBar.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import logo from '../logo.png';
+import Navbar from 'react-bootstrap/NavBar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+
+export function NavBar() {
+
+  return (
+      <div>
+        <Navbar bg="light" expand="lg">
+          <Container fluid>
+            <img src={logo} className="App-logo" alt="logo" />
+            <Navbar.Brand href="#">Fleamarket & other stories</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+              <Nav
+                className="me-auto my-2 my-lg-0"
+                style={{ maxHeight: '100px' }}
+                navbarScroll
+              >
+                <Nav.Link href="#action1">Home</Nav.Link>
+                <Nav.Link href="#action2">Clothes</Nav.Link>
+                <Nav.Link href="action3">Furniture</Nav.Link>
+                <NavDropdown title="Others" id="navbarScrollingDropdown">
+                  <NavDropdown.Item href="#action3">Tecnology</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">Home appliances</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action5">
+                    Something else here
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+              <Form className="d-flex">
+                <FormControl
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+              </Form>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div> 
+  )
+}
