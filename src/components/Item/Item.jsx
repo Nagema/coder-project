47d8736e71af16409bde79adf417d575c0a66728
angelica <angelica.cavero@gmail.com>
2022-07-05 19:56:07 +0200

    useNavigate

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,30 +1,39 @@
 import React from 'react'
 import { ItemCount } from '../ItemCount/ItemCount';
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Badge from 'react-bootstrap/Badge';
+import { useNavigate } from 'react-router-dom';
 
-export function Item({
-    title,  
-    image, 
-    stock, 
-    price, 
+export function Item({  
+    stock,  
     currency,
-    goToDetail 
+    goToDetail,
+    product 
 }) {
+
+  const navigate = useNavigate();
+  const handleDetail = () => {
+    navigate(`/detail/${product.id}`)
+  }
   return (
     <Card style={{ width: '15rem' }}>
         <Card.Img 
           variant="top" 
-          src={image} 
+          src={product.image} 
           style={{ width: '8rem', height: '12rem', margin: 'auto', paddingTop: '20px' }} 
         />
         <Card.Body>  
-            <Card.Title>{title}</Card.Title>
+            <Card.Title>{product.title}</Card.Title>
             <ItemCount   
               stock={stock}
-              price={price}
+              price={product.price}
               currency={currency}
-              goToDetail={goToDetail}
             />
+            <div className='buying-process'>
+              <Button size="sm" variant="warning">add to cart</Button>
+              <Button size="sm" variant="light" onClick={handleDetail}>{goToDetail}</Button>
+          </div>
         </Card.Body>
     </Card>
   )
