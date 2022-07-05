480811558913cfae0fa43e00e8ef709e9950b356
angelica <angelica.cavero@gmail.com>
2022-07-05 19:56:41 +0200

    useParams

--- src/containers/ItemDetailContainer/index.jsx
+++ src/containers/ItemDetailContainer/index.jsx
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from 'react'
 import ItemDetail from '../../components/ItemDetail/ItemDetail';
+import { useParams } from 'react-router-dom'
 
 function ItemDetailContainer() {
 
   const [productDetail, setProductdetail] = useState({});
+  const params = useParams();
 
   useEffect(()=>{
       const getDetailProducts = async () => {
         try {
-          const response = await fetch('https://fakestoreapi.com/products/1');
+          const response = await fetch(`https://fakestoreapi.com/products/${params.productId}`);
           const data = await response.json();
           setProductdetail(data)
         } catch {
@@ -16,7 +18,7 @@ function ItemDetailContainer() {
         }
       }
       getDetailProducts();
-  }, [])
+  }, [params])
 
   return (
     <ItemDetail product={productDetail}/>
