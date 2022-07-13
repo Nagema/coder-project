ca78ca484f2bc20e7897c36323b2fe7288ec68d8
angelica <angelica.cavero@gmail.com>
2022-07-13 22:43:54 +0200

    small fixes

--- src/containers/Cart/Cart.jsx
+++ src/containers/Cart/Cart.jsx
@@ -9,17 +9,21 @@ import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 function Cart() {
 
-  
   const { cart, removeItem, clear } = useContext(Shop);
-
+  const currency = 'EUR';
+  const qtyText = 'Qty';
+  const deleteText = 'Delete';
+  const deleteAllText = 'Delete all'
   return (
     <div className="cardGroupWrap">
       {cart.length > 0 &&
+      <div className="deleteAllButton">
         <Button variant="light" onClick={clear}> 
-        <span>
-          Delete all <FontAwesomeIcon icon={faTrash} size="lg" />
-        </span>    
-      </Button>
+          <span>
+            {deleteAllText} {<FontAwesomeIcon icon={faTrash} size="lg" />}
+          </span>    
+        </Button>
+      </div>
       }
       <Row sx={1} md={1} className="g-4">
         {cart.map((product) => (
@@ -32,9 +36,11 @@ function Cart() {
                     />
                   <Card.Body>  
                   <Card.Title>{product.title}</Card.Title>
-                  <Card.Text>{product.addedProducts}</Card.Text>
+                  <Card.Title>{product.price} {currency}</Card.Title>
+                  <Card.Text>{qtyText} : {product.addedProducts}</Card.Text>
                   <Button variant="light" onClick={() => removeItem(product, 1)}> 
-                    <FontAwesomeIcon icon={faTrash} size="lg" />
+                    <span> {deleteText} {<FontAwesomeIcon icon={faTrash} size="lg" />}
+                    </span>
                   </Button>
                   </Card.Body>
                 </div>
